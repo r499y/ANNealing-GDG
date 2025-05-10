bbe819ff7084652e5866e9d031e0766d43bfec19
GiuseppePisante <giuseppe.pisante@mail.polimi.it>
2025-05-10 15:28:39 +0000

    PopUp Icon working let's go

---
+++ FrontEnd/icon/popup-agent.py
@@ -0,0 +1,8 @@
+import subprocess
+
+subprocess.run([
+    "/mnt/c/Windows/System32/cmd.exe",
+    "/c",
+    "C:\\Users\\gpisa\\AppData\\Roaming\\npm\\electron.cmd",
+    "C:\\Users\\gpisa\\Desktop\\Hackathon\\popup-agent"
+])
